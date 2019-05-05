caae981ec2e53eaf6b76c1c75e924e0edc377840
Fernando Takahama Tsuhaya <fernandotsuhaya@gmail.com>
2019-05-04 21:35:35 -0300

    route

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -8,8 +8,8 @@ import Signup from './pages/Signup/signup';
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path='/' component={Signin} />
-            <Route path='/main' component={Main} />
+            <Route exact path='/' component={Main} />
+            <Route path='/signin' component={Signin} />
             <Route path='/signup' component={Signup} />
             <Redirect from='*' to='/' />
         </Switch>
